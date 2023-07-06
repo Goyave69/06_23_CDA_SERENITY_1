87711c0cff3417c84c9c292842a0664677db7c3f
idkimaya <112517716+idkimaya@users.noreply.github.com>
2023-07-06 12:35:24 +0200

    fixed files

--- frontend/src/components/preparationcomponents/Appointment.jsx
+++ frontend/src/components/preparationcomponents/Appointment.jsx
@@ -1,19 +1,6 @@
 import { Button, Typography } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import ButtonBase from '@mui/material/ButtonBase';
-
-const Img = styled('img')({
-  margin: 'auto',
-  display: 'block',
-  maxWidth: '100%',
-  maxHeight: '100%',
-});
-
 
 export default function Appointment() {
   return (
