d77c34b20448a07d61e99a4b28f217d3ef7484f7
idkimaya <112517716+idkimaya@users.noreply.github.com>
2023-07-06 11:54:58 +0200

    added appointment

--- frontend/src/components/preparationcomponents/Appointment.jsx
+++ frontend/src/components/preparationcomponents/Appointment.jsx
@@ -1,7 +1,45 @@
+import { Button, Typography } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
+
+const Img = styled('img')({
+  margin: 'auto',
+  display: 'block',
+  maxWidth: '100%',
+  maxHeight: '100%',
+});
+
+
 export default function Appointment() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <Card
+        sx={{
+          border: "3px solid #66E47A",
+          marginTop: "40px",
+          backgroundColor: "#F5F5F5",
+          borderRadius: "20px",
+        }}
+      >
+        <CardContent>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              fontSize: 14,
+              display: "flex",
+              justifyContent: "start",
+              marginBottom: "48px",
+            }}
+          >
+            Les Rendez-vous disponible
+          </Typography>
+          
+        </CardContent>
+      </Card>
   );
 }
