ca294b3021a50084828c9c25938207377e43d184
Missblue <mireille.dert@gmail.com>
2023-07-17 21:31:56 +0200

    crud office

--- frontend/src/pages/admin/OfficesList copy.jsx
+++ frontend/src/pages/admin/OfficesList copy.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+/* import React, { useState, useEffect, useMemo } from "react";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
@@ -42,7 +42,7 @@ const OfficesList = () => {
         marginLeft: "150px",
       }}
     >
-      {/* <h3>Liste des Cabinets dans lesquels vous pouvez prendre RDV</h3> */}
+      {<h3>Liste des Cabinets dans lesquels vous pouvez prendre RDV</h3>}
       
       <Box
           sx={{
@@ -82,95 +82,4 @@ const OfficesList = () => {
 }
 
 export default OfficesList;
-
-/* import React, { useEffect, useMemo, useState }from 'react';
-import Container from "@mui/material/Container";
-import { DataGrid } from "@mui/x-data-grid";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import { NavLink } from "react-router-dom";
-import axios from "axios";
-
-
-
-const ListeCabinets = () => {
-
-useEffect(() => {
-  axios.get("http://localhost:5000/offices/").then((response) => {
-    console.log("response", response);
-    //setData(response.data);
-  });
-}), [];
-
-
-  const [data, setData] = useState([]);
-  const columns = useMemo(
-    () => [
-      {
-        field: "Sélection",
-        headerName: "Sélection",
-        headerClassName: "green-header",
-        width: 190,
-      },
-      {
-        field: "Praticiens",
-        headerName: "Praticiens",
-        width: 150,
-        editable: true,
-      },
-      { field: "Patients", headerName: "Patients", width: 150, editable: true },
-      { field: "Interventions", headerName: "Interventions", width: 150,
-      editable: true,},
-      { field: "Satisfaction", headerName: "Satisfaction", width: 150,
-      editable: true, },
-    ],
-    []
-  );
-
-  
-    return (
-      <Container
-        maxWidth="sm"
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          minHeight: "80vh",
-          marginLeft: "150px",
-        }}
-      >
-        <Box
-          sx={{
-            p: 4,
-            boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-            borderRadius: "4px",
-            width: "auto",
-          }}
-        >
-          <Box sx={{ width: "100%" }}>
-            <DataGrid
-              columns={columns}
-              rows={data}
-              checkboxSelection
-              autoHeight
-              disableColumnMenu
-            />
-          </Box>
-          <Box display="flex" justifyContent="center" mt="2rem">
-            <NavLink to="/admin/add-offices">
-              <Button
-                variant="contained"
-                color="primary"
-                sx={{ borderRadius: "10px", marginTop: "20px" }}
-              >
-                Nouveau cabinets
-              </Button>
-            </NavLink>
-          </Box>
-        </Box>
-      </Container>
-    );
-};
-
-export default Cabinets; */
+ */
