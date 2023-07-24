891b107bc6575eeb95bbedfc7d45b63560d87d6f
idkimaya <112517716+idkimaya@users.noreply.github.com>
2023-07-24 12:00:24 +0200

    added calendar2

--- frontend/src/pages/TimeTable.jsx
+++ frontend/src/pages/TimeTable.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import * as React from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import Box from '@mui/material/Box';
 
-export default function TimeTable() {
+export default function StaticDatePickerLandscape() {
   return (
-    <div>
-      <p> This is Agenda!</p>
-    </div>
+    <Box sx={{ml:5, mt:16, boxShadow:3, borderRadius:"10px"}}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <StaticDatePicker orientation="landscape" sx={{borderRadius:"10px"}} />
+      </LocalizationProvider>
+    </Box>
   );
 }
